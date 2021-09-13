79a3b0cade265c4ea46627e47745cf74cea4b017
fayyozbobokulov <b.fayyoz2000@gmail.com>
2021-09-13 19:03:00 +0500

    imported mongodb

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongodb_1 = require("mongodb");
+console.log(mongodb_1.MongoClient);
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import {MongoClient} from "mongodb";
+
+console.log(MongoClient);
+
+
+
